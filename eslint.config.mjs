c967e22abc569db1ee6456a6ba333f8cbe5af601
Ali Sunjaya <ali.sunjayaa@gmail.com>
2026-09-02 13:43:50 +0700

    feat: add Prettier configuration to ESLint setup

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,9 @@
 // @ts-check
 import withNuxt from './.nuxt/eslint.config.mjs'
+import eslintConfigPrettier from 'eslint-config-prettier'
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended'
 
 export default withNuxt(
   // Your custom configs here
+  [eslintConfigPrettier, eslintPluginPrettierRecommended],
 )
